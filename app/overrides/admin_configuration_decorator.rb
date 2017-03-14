a96964ae31e84a409ce376b920ff60099ff39d57
jakubwilczek <jakub.wilczek@netguru.co>
2017-03-14 17:46:58 +0100

    change data-hook

--- app/overrides/admin_configuration_decorator.rb
+++ app/overrides/admin_configuration_decorator.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(virtual_path: 'spree/admin/shared/_tabs',
+Deface::Override.new(virtual_path: 'spree/admin/shared/_settings_sub_menu',
                      name: 'add_social_providers_link_configuration_menu',
                      insert_bottom: '[data-hook="admin_settings_sub_tabs"]',
                      text: '<%= configurations_sidebar_menu_item Spree.t(:social_authentication_methods), spree.admin_authentication_methods_path %>',
