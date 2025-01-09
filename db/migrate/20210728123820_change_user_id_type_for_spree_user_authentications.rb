8d1c61ff40c09905453730b4bf7af66ff4b0b74d
Rahul <rahul97811@gmail.com>
2025-01-09 23:59:17 +0530

    Revert user_id type change in spree_user_authentications for foreign key compatibility

--- db/migrate/20210728123820_change_user_id_type_for_spree_user_authentications.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class ChangeUserIdTypeForSpreeUserAuthentications < SolidusSupport::Migration[4.2]
-  def change
-    change_column :spree_user_authentications, :user_id, :bigint
-  end
-end
