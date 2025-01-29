0c7e0d37ea3036348e92f7123b744ec8b8c32b07
Rahul <rahul97811@gmail.com>
2025-01-29 22:02:04 +0530

    Merge guest user cart with authenticated one after social login
    Use `set_current_order` after login via social auth to ensure the cart
    from guest user should merge into authentication one after login.

--- app/controllers/spree/admin/authentication_methods_controller.rb
+++ app/controllers/spree/admin/authentication_methods_controller.rb
@@ -3,6 +3,8 @@
 module Spree
   module Admin
     class AuthenticationMethodsController < ResourceController
+      private
+
       def build_resource
         model_class.new(
           store_ids: [Spree::Store.default.id].compact
--- spec/features/spree/sign_in_spec.rb
+++ spec/features/spree/sign_in_spec.rb
@@ -190,4 +190,73 @@ RSpec.describe 'Signing in using Omniauth', :js do
       expect(page).to have_text 'Welcome! You have signed up successfully.'
     end
   end
+
+  context 'Merge guest user cart with authenticated one' do
+    before do
+      Spree::AuthenticationMethod.create!(
+        provider: 'google_oauth2',
+        api_key: 'fake',
+        api_secret: 'fake',
+        environment: Rails.env,
+        active: true
+      )
+      OmniAuth.config.mock_auth[:google_oauth2] = {
+        'provider' => 'google_oauth2',
+        'uid' => '123545',
+        'info' => {
+          'name' => 'mockuser',
+          'email' => 'mockuser@example.com',
+          'image' => 'mock_user_thumbnail_url'
+        },
+        'credentials' => {
+          'token' => 'mock_token',
+          'secret' => 'mock_secret'
+        }
+      }
+
+      # Create a default store
+      Spree::Store.create!(
+        name: 'Default Store',
+        code: 'default',
+        url: 'www.example.com',
+        mail_from_address: 'store@example.com',
+        default_currency: 'USD',
+        default: true
+      )
+
+      # Create a product
+      @product = create(:product)
+    end
+
+    it 'merges guest and user orders after login' do
+      # Step 1: Login and create an order with quantity 1
+      visit spree.login_path
+      click_on 'Login with google_oauth2'
+      expect(page).to have_text 'You are now signed in with your google_oauth2 account.'
+
+      # Add a product to the cart with quantity 1
+      visit spree.product_path(@product)
+      find('#add-to-cart-button').click
+
+      user_order = Spree::Order.last
+      expect(user_order.item_count).to eq(1)
+      # Logout
+      click_link 'Logout'
+
+      # Step 2: As a guest, add a product to the cart with quantity 2
+      visit spree.product_path(@product)
+      fill_in 'quantity', with: 2
+      find('#add-to-cart-button').click
+
+      guest_order = Spree::Order.last
+      expect(guest_order.item_count).to eq(2)
+
+      # Step 3: Login again and verify the order is merged
+      visit spree.login_path
+      click_on 'Login with google_oauth2'
+      expect(page).to have_text 'You are now signed in with your google_oauth2 account.'
+      merged_order = Spree::Order.last
+      expect(merged_order.item_count).to eq(3)
+    end
+  end
 end
