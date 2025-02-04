0d25953fdbe5c4f056cbae013972c9d578816df9
Rahul <rahul97811@gmail.com>
2025-02-05 02:19:13 +0530

    Update setting menu label to social auth

--- config/initializers/add_authentication_methods_to_settings_menu.rb
+++ config/initializers/add_authentication_methods_to_settings_menu.rb
@@ -3,9 +3,9 @@
 Spree::Backend::Config.configure do |config|
   settings_menu = config.menu_items.find { |item| item.label == :settings }
 
-  if settings_menu
+  if settings_menu && settings_menu.respond_to?(:children)
     settings_menu.children << config.class::MenuItem.new(
-      label: :social_authentication_methods,
+      label: :social_auth,
       url: :admin_authentication_methods_path,
       condition: -> { can?(:admin, Spree::AuthenticationMethod) }
     )
